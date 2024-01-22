05d02c40cf0d47e9adca54136d18fe625f9391a9
mwangigitau <willitimgitau@gmail.com>
2024-01-22 12:45:36 +0300

    A bit on torrenting

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
+    
--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,9 @@
 import base64
 import requests
 from datetime import datetime
+import libtorrent as lt
+import time
+import sys
 
 def get_access_token():
     url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
@@ -36,7 +39,7 @@ def generate_qr_code():
         "CPI": "373132",
         "Size": "300"
     }
-    token = {"access_token": ""}
+    token = {"access_token": "EmpKFWVqLRADAusgudk6rY7a4wj0"}
 
     headers = {
         "Content-Type": "application/json",
@@ -120,6 +123,35 @@ def register_urls():
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         raise
+
+def download_torrent():
+
+
+    ses = lt.session({'listen_interfaces': '0.0.0.0:6881'})
+
+    magnet_link = "magnet:?xt=urn:btih:158E3514A8EF7A538F4BE3075504612241ED6C62&dn=Terraform+Associate+Certification+%2B300+Exam+Practice+Questions...2ed+2023&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=ud"
+    info = lt.torrent_info(lt.parse_magnet_uri(magnet_link))
+    h = ses.add_torrent({'ti': info, 'save_path': '.'})
+    s = h.status()
+    print('starting', s.name)
+
+    while (not s.is_seeding):
+        s = h.status()
+
+        print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
+            s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
+            s.num_peers, s.state), end=' ')
+
+        alerts = ses.pop_alerts()
+        for a in alerts:
+            if a.category() & lt.alert.category_t.error_notification:
+                print(a)
+
+        sys.stdout.flush()
+
+        time.sleep(1)
+
+print(h.status().name, 'complete')
 if __name__ == '__main__':
     register_urls()
 
